7bd46ba1be187de1a32ce871b0a2ce9cbc559d05
Vaibhav <vaibhavovhal15@gmail.com>
2023-05-25 12:26:55 +0530

    [Vaibhav] Add:Validating first name

---
+++ UserRegistrations/UserRegistrations/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UserRegistrations
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration");
+
+            Console.WriteLine("\n==============||===============\n");
+
+            Registration reg = new Registration();
+            reg.Register();
+        }
+    }
+}
---
+++ UserRegistrations/UserRegistrations/Registration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrations
+{
+    public class Registration
+    {
+        public void Register()
+        {
+            string pattern_name = @"^[A-Z][a-z]{2,}";
+            string input_name = "";
+
+            Regex obj = new Regex(pattern_name);
+
+            while (true)
+            {
+                Console.Write("Please enter a name: ");
+                input_name = Console.ReadLine();
+
+                if (obj.IsMatch(input_name))
+                {
+                    Console.WriteLine($"'{input_name}' is a valid User Name.\n");
+                }
+                else
+                {
+                    Console.WriteLine("Invailid Input!! \nTry Again...\n");
+                }
+            }
+        }
+    }
+}
